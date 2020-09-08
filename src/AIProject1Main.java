ce5da014a5f181a5ce70d306fa87b479286094f8
Ru Ferguson <ruf@smu.edu>
2020-09-08 17:35:35 -0500

    commented and consolidated code for style. ready to turn in part 1.

--- src/AIProject1Main.java
+++ src/AIProject1Main.java
@@ -1,10 +1,11 @@
-/*
- * c2017-2019 Courtney Brown 
+/* Ru Ferguson
+ * 8 September 2020
  * 
- * Class: H
- * Description: Demonstration of MIDI file manipulations, etc. & 'MelodyPlayer' sequencer
+ * This program will play Mary Had a Little Lamb using the Kontakt MIDI Player and will store 
+ * the notes used, the occurrences of each note, and the probabilities of each note occurring 
+ * in ArrayLists using the ProbabilityGenerator class. 
  * 
- */
+ * Press "1" to activate the Unit Test 1 */
 
 import processing.core.*;
 
@@ -21,7 +22,7 @@ import java.net.*;
 
 //import javax.sound.midi.*;
 
-			//make sure this class name matches your file name, if not fix.
+//make sure this class name matches your file name, if not fix.
 public class AIProject1Main extends PApplet {
 
 	MelodyPlayer player; //play a midi sequence
@@ -30,15 +31,12 @@ public class AIProject1Main extends PApplet {
 	ProbabilityGenerator<Double> rhythmGenerator;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		PApplet.main("AIProject1Main"); //change this to match above class & file name 
-
+		PApplet.main("AIProject1Main"); 
 	}
 
 	//setting the window size to 300x300
 	public void settings() {
 		size(300, 300);
-
 	}
 
 	//doing all the setup stuff
@@ -55,31 +53,34 @@ public class AIProject1Main extends PApplet {
 		// playMidiFile(filePath);
 
 		midiNotes = new MidiFileToNotes(filePath); //creates a new MidiFileToNotes -- reminder -- ALL objects in Java must 
-												//be created with "new". Note how every object is a pointer or reference. Every. single. one.
-
+												   //be created with "new". Note how every object is a pointer or reference. Every. single. one.
 
 	    // which line to read in --> this object only reads one line (or ie, voice or ie, one instrument)'s worth of data from the file
 		midiNotes.setWhichLine(0);
 
+		// call the train function for both pitches and rhythms
 		pitchGenerator.train(midiNotes.getPitchArray());
 		rhythmGenerator.train(midiNotes.getRhythmArray());
 
-		
 		player = new MelodyPlayer(this, 100.0f);
 		
 		player.setup();
-		//player.setMelody(pitchGenerator.generate(20));
-		//player.setRhythm(rhythmGenerator.generate(20));
+
 		player.setMelody(midiNotes.getPitchArray());
 		player.setRhythm(midiNotes.getRhythmArray());
+		
+		/* generate() is a work in progress
+		 * player.setMelody(pitchGenerator.generate(20)); 
+		 * player.setRhythm(rhythmGenerator.generate(20)); */
 	}
 
 	public void draw() {
 		player.play(); //play each note in the sequence -- the player will determine whether is time for a note onset
 
-		/*textSize(12);
-		fill(0, 102, 153);
-		text("Press 1 to start the unit test!", width/2, height/2);*/
+		textAlign(CENTER);
+		textSize(18);
+		fill(150, 0, 255);
+		text("Press 1 to start the unit test!", width/2, height/2); // display instructions to the user
 	}
 
 	//this finds the absolute path of a file
@@ -96,9 +97,9 @@ public class AIProject1Main extends PApplet {
 		return filePath;
 	}
 
-	//this function is not currently called. you may call this from setup() if you want to test
-	//this just plays the midi file -- all of it via your software synth. You will not use this function in upcoming projects
-	//but it could be a good debug tool.
+	// this function is not currently called. you may call this from setup() if you want to test
+	// this just plays the midi file -- all of it via your software synth. You will not use this
+	// function in upcoming projects but it could be a good debug tool.
 	void playMidiFile(String filename) {
 		Score theScore = new Score("Temporary score");
 		Read.midi(theScore, filename);
@@ -107,7 +108,7 @@ public class AIProject1Main extends PApplet {
 
 	//this starts & restarts the melody.
 	public void keyPressed() {
-		// ProbabilityGenerator<Integer> pitchGenerator = new ProbabilityGenerator<Integer>();
+		// ProbabilityGenerator<Integer> pitchGenerator = new ProbabilityGenerator<Integer>(); WORK IN PROGRESS
 
 		MidiFileToNotes midiNotesMary; // read a midi file
 		// returns a url
@@ -139,9 +140,6 @@ public class AIProject1Main extends PApplet {
 				rhythmGenerator.getProbability(i));
 			}
 			System.out.println("\n------------\n");
-
-			System.out.println(midiNotesMary.getPitchArray());
-			System.out.println(midiNotesMary.getRhythmArray());
 		}
 	}
 }
--- src/ProbabilityGenerator.java
+++ src/ProbabilityGenerator.java
@@ -1,73 +1,65 @@
+/* Ru Ferguson
+ * 8 September 2020
+ * 
+ * This class will store the musical notes and the number of occurences of each note
+ * using the train() method and will generate and output the next most probable note
+ * using the generate() method. */
+
 import java.util.ArrayList;
-
-// comment the heck out of everything here
-// Programmer: Ru Ferguson
-// 9/2/2020
-// Description
-/* Your Project 1: 
-
-	With train() & unit 1 working
-
-	30% --> Style -- 7.5% each numbered item 
-		1. Principle of maximum re-use / code smallness
-				Code is not repeated (eg. methods or loops are used appropriately)
-		
-		2. Principle of encapsulation
-				Methods are no more than 35 lines long. 
-				Data is accessed in classes via get/set
-				Each method and class does or represents just one thing
-		
-		3. Principle of prototype iteration
-				2 git commits on 2 separate days not including the day of the deadline 
-		
-		4. Comments and Naming Conventions
-				Class header for each class/file -- name/date/description. My name should not be in the main class. 
-				Classes and other variables are named descriptively. 
-				Every method and function is commented with what it does and what each parameter is
-	35% train() works
-	30% unit test 1 works
-	5% unit test 1 runs from the GUI as per Project #1 guidelines.
-*/
 
 public class ProbabilityGenerator<T> {
 	
 	
 	ArrayList<T> alphabet;
 	ArrayList<Integer> alphabet_counts;
+	ArrayList<Double> probabilities;
 	
 	ProbabilityGenerator() {
 		alphabet = new ArrayList<T>();
 		alphabet_counts = new ArrayList<Integer>();
+		probabilities = new ArrayList<Double>();
 	}
 	
+	// returns the size of the alphabet ArrayList
 	public int getAlphabetSize() {
 		return alphabet.size();
 	}
 	
+	// returns the ArrayList containing the counts of occurrences for each note
 	public ArrayList<Integer> getAlphabetCounts() {
 		return alphabet_counts;
 	}
 	
+	// returns the token at a given index in the ArrayList alphabet
 	public T getToken(int index) {
 		return alphabet.get(index);
 	}
 	
+	// returns the probability from the probabilities ArrayList at a specific index 
 	public double getProbability(int index) {
-		double answer = alphabet_counts.get(index) / getTotalCount();
-		return answer;
+		getProbabilities();
+		return probabilities.get(index);
 	}
 	
-	public double getTotalCount() {
-		double answer = 0;
+	// returns the total number of notes
+	public double getTotal() {
+		double total = 0;
 		for (int i = 0; i < alphabet_counts.size(); i++) {
-			answer = answer + alphabet_counts.get(i);
+			total = total + alphabet_counts.get(i);
 		}
-		return answer;
+		return total;
 	}
 	
-	// it is training probability generator with new data
+	// adds the probabilities to an ArrayList called probabilities
+	public ArrayList<Double> getProbabilities() { 
+		for (int i = 0; i < alphabet_counts.size(); i++) {
+			probabilities.add((double) alphabet_counts.get(i) / getTotal()); 
+		}
+		return probabilities;
+	}
+	
+	// adds new tokens to the alphabet ArrayList and counts number of occurrences in the alphabet_counts ArrayList
 	void train(ArrayList<T> newTokens) {
-		// you will code the training function that will add new Tokens to alphabet
 		for (int i = 0; i < newTokens.size(); i++) {
 			int index = alphabet.indexOf(newTokens.get(i));
 			// if new token is not in the alphabet array, add a new ArrayList item with the new token
@@ -82,54 +74,37 @@ public class ProbabilityGenerator<T> {
 					alphabet_counts.set(j, tempCount);
 				}
 			}
-			}
-		 /* NOTE: at some point you will want to normalize your counts – ie. divide by total
-		    & get values that add to 1. In this version of the algorithm, we do this in
-		    generate(). However, if working with larger sets of data, you may want to do this
-		    in this step after each train & store normalized values to prevent overflow problems.
-		    However, for this project and for assigned projects, this is unlikely, so feel free
-		    to skip until you enter in an enormous amount of data yourself and the numbers don’t
-		    add up. Live dangerously, is what I am saying */
+		}
 	}
 	
 	
+	// WORK IN PROGRESS
 	T generate() {
 		T newToken = null;
 		
-		// actual java code. yw!! Note that you will not be able to use processing code
-		// inside your class unless you somehow reference your main class. 
-		double totalNotes = 0;
-		for (int i = 0; i < alphabet.size() - 1; i++) {
-			double currentNote = (double) alphabet.get(i);
-			totalNotes = totalNotes + currentNote;
-		}
+		double rIndex = (double) Math.random();
 		
-		ArrayList<Double> normalized = new ArrayList<Double>();
-		for (int i = 0; i < alphabet_counts.size(); i++) {
-			normalized.add((double) alphabet_counts.get(i) / totalNotes); 
-		}
-
-		
-		float rIndex = (float) Math.random();
-		if (rIndex < normalized.get(1)) {
+		/*
+		if (rIndex < probabilities.get(probabilities.size())) {
 			// then a
 			System.out.println("a");
-		} else if (rIndex < normalized.get(2)) {
+		} else if (rIndex < probabilities.get(probabilities.size() - 1)) {
 			// then b
 			System.out.println("b");
-		} else if (rIndex < normalized.get(3)) {
+		} else if (rIndex < probabilities.get(probabilities.size() - 2)) {
 			// then c
 			System.out.println("c");
-		} else if (rIndex < normalized.get(4)) {
+		} else if (rIndex < probabilities.get(probabilities.size() - 3)) {
 			// then d
 			System.out.println("d");
 		} 
+		*/
 		
 		// do something here
 		return newToken;
 	} 
 	
-	ArrayList<T> generate( int length ) {
+	ArrayList<T> generate(int length) {
 		ArrayList<T> newSequence = new ArrayList<T>();
 		
 		for (int i = 0; i < length; i++) {
